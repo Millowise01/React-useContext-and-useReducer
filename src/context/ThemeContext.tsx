e8298f4a3e3d1071b453af9a6fccf88cf7c38997
Millowise01 <l.carew@alustudent.com>
2026-09-24 18:24:20 +0300

    wire ThemeProvider and compose Navbar with TaskManager in App tsx

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from "react";
+import { createContext, useContext, useState, type ReactNode } from "react";
 import { LIGHT_THEME, DARK_THEME } from "../constants/theme";
 
 // The theme can only ever be one of the two constant values.
